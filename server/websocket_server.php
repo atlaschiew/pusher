9bb4d493656acd94e1a6c1472a08316852d761a9
madxradicle <45816141+madxradicle@users.noreply.github.com>
2019-06-28 17:06:18 +0800

    Create websocket_server.php

---
+++ server/websocket_server.php
@@ -0,0 +1,37 @@
+<?php
+/*
+[MADX WS API]
+WEBSOCKET_OPCODE_BINARY is prohibited
+
+91zb3cHS;9Y3
+
+[TOOLS]
+/opt/cpanel/ea-php70/root/usr/bin/php /home/madxws/swoole/websocket_server.php
+pstree -ap|grep websocket_server.php
+kill -15 <master PID>
+kill -USR1 <manager PID>, restart worker
+
+[PROTOCOL]
+0                   1                   2                   3
+0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
++-+-+-+-+-------+-+-------------+-------------------------------+
+|F|R|R|R| opcode|M| Payload len |    Extended payload length    |
+|I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
+|N|V|V|V|       |S|             |   (if payload len==126/127)   |
+| |1|2|3|       |K|             |                               |
++-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
+|     Extended payload length continued, if payload len == 127  |
++ - - - - - - - - - - - - - - - +-------------------------------+
+|                               |Masking-key, if MASK set to 1  |
++-------------------------------+-------------------------------+
+| Masking-key (continued)       |          Payload Data         |
++-------------------------------- - - - - - - - - - - - - - - - +
+:                     Payload Data continued ...                :
++ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+|                     Payload Data continued ...                |
++---------------------------------------------------------------+
+*/
+chdir(dirname(__FILE__));
+include_once "common.php";
+
+new \MadxWS\Main($argv,$vars);
